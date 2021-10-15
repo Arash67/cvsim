fa06882d1c2dd19b9fca80f6daee29e0611a21b6
Arash GH <51331180+Arash67@users.noreply.github.com>
2021-10-15 01:34:16 -0500

    Update control_point_manipulation.py

--- modules/control_point_manipulation.py
+++ modules/control_point_manipulation.py
@@ -39,17 +39,13 @@ def scale_factor_test(length_id,scale_id,discrt_id,long_asym_id,num_contours,con
     for i in range(prox_cont_num):
         proximal_scale_list.append(float(coef_a*common_ratio**power))
         power                       -= 1
-    print(min(proximal_scale_list))
-    scale2scale_id                  = scale_id / min(proximal_scale_list)
-    proximal_scale_list             = [element * scale2scale_id for element in proximal_scale_list]
-    print("Proximal scale factors: {0:d}".format(proximal_scale_list))
+   
     power                           = distal_cont_num
     for i in range(dist_cont_num):
         distal_scale_list.append(float(coef_a*common_ratio**power))
         power                       -= 1 
-    print(min(distal_scale_list))
-    scale2scale_id                  = scale_id / min(distal_scale_list)
-    distal_scale_list               = [element * scale2scale_id for element in distal_scale_list]
+    
+    print("Proximal scale factors: {0:d}".format(proximal_scale_list))
     print("Distal scale factors: {0:d}".format(distal_scale_list))
     
 def radial_expansion_test(center,new_outer_points,dists,unit_vectors,scale_factor):
