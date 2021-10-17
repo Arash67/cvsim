9b8384b0b861ed96c7b823dd933fb33827402c8d
Arash GH <51331180+Arash67@users.noreply.github.com>
2021-10-17 00:37:02 -0500

    Update control_rabbit_generalized.py

--- geometry/control_rabbit_generalized.py
+++ geometry/control_rabbit_generalized.py
@@ -96,7 +96,7 @@ seg_fulldir                 = cvsim + input_dir + seg_name
 length_id                   = 6
 # diameter reduction at most stenotic segment
 perc_diameter_reduction     = 80
-scale_id                    = 1-(perc_diameter_reduction/100)
+scale_id                    = perc_diameter_reduction/100
 # longitudinal asymetry (-1 indicating narrowing toward proximal, 0 indicating symmetric narrowing, 1 toward the distal)
 # long_asym_id                = -0.2
 # long_asym_id                = 0.2
