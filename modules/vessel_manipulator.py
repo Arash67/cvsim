32f1112a8c1295f98849574beee8d21b27fed337
Arash GH <51331180+Arash67@users.noreply.github.com>
2021-10-21 18:00:20 -0500

    Update vessel_manipulator.py

--- modules/vessel_manipulator.py
+++ modules/vessel_manipulator.py
@@ -329,7 +329,7 @@ def manipulator(vessel_id,cvsim,input_dir,cvsimout,seg_name,vessel_par,mesh_par)
     loft_capped                         = loft(vessel_id,contours_manip,cvsimout)
     remesh(loft_capped,cvsimout)
     # mesh
-    do_mesh(vessel_id,cvsimout,"capped-loft-surface.vtp",mesh_par)
+    do_mesh(vessel_id,cvsimout,vessel_id + "_capped-loft-surface.vtp",mesh_par)
     
     # Draw segmentation
     # draw_segmentations(cvsim,contours_manip)
