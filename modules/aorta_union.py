e991bd78e3fbdedbc1d5bdb249c2c5b507b02c66
Arash GH <51331180+Arash67@users.noreply.github.com>
2021-10-21 21:58:15 -0500

    Create aorta_union.py

---
+++ modules/aorta_union.py
@@ -0,0 +1,21 @@
+
+# A:======================================================= GENERAL
+# The purpose is to create a boolean union model of the aorta and branches 
+# 
+# NOTE: this script assumes the cvsim repository is mounted on an ubuntu os in the following directory: (/home/ubuntu_username/github/) please see line 38 as an example
+# NOTE: for information regarding how to mount github repository on local computer please read the git_installation.txt file in cvsim/documentations/ubuntu/git_installation.txt
+
+# NOTE: You will need SimVascular installed on Ubuntu (for windows users its most easy through hyper-v manager),
+# NOTE: to install ubuntu on hyper-v please see the instructions in hyperv_instructions.txt at cvsim/documentations/mswindows/hyperv_instructions.txt
+# NOTE: additional instructions are included within the script
+
+
+#--------------------------------------------------------
+# ABREVIATIONS:
+#               Aorta                               (AO)
+
+#--------------------------------------------------------
+
+# B:======================================================= START
+
+import sv
