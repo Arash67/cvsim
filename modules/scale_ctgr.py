6225b9d56317fc5ac4ed978753b16cea180c4bcd
Arash GH <51331180+Arash67@users.noreply.github.com>
2021-10-25 00:24:20 -0500

    Update scale_ctgr.py

--- modules/scale_ctgr.py
+++ modules/scale_ctgr.py
@@ -41,7 +41,87 @@ class Contour(object):
 
 # D:======================================================= FUNCTIONS
 
-def read_ctgr_file(file_name, contour_scale, contour_ids):
+def sigmoid(scale_factor,number_of_contours,current_contour_number,steepness,x50):
+	# L : amplituded must be between indicating diameter reduction (between 0 and one) or expantion ratio (larger than one)
+	L                                   = scale_factor
+	# k : steepness 
+	k                                   = steepness 
+	# x : current disrtance from the left end
+	x                                   = current_contour_number
+	# x0: x at 50% drop used to include longitudinal asymetry of how close to the narrwoing the 50% drop is current assumtion is scaled range of 12 woth 50% happening at 6
+	# x50                                 = 0.2				
+	x0                                  = number_of_contours * x50
+	
+	print(x-x0)
+	if scale_factor < 1:
+		# contraction
+		temp                            = 1 - (L / (1 + math.exp(-k*(x-x0))))
+	else:
+		# explantion
+		temp                            = 1 + ((L-1) / (1 + math.exp(-k*(x-x0))))
+	return temp 
+
+def scale_factor_insert(scale_factor,scale_factor_local,start_id,stop_id):
+	indx_counter = 0
+	for sid in range(len(scale_factor)-1):
+		if sid > (start_id -1):
+			if sid < stop_id-1:
+				scale_factor[sid]           = scale_factor_local[indx_counter]
+				indx_counter            += 1
+			else:
+				scale_factor[sid]		= 1
+		else:
+			scale_factor[sid]			= 1
+	print("number of updated contours: {0:d}".format(indx_counter))
+	return scale_factor
+
+def scale_factor_test(number_of_contours,scale_par):
+    length_id = scale_par[0]
+    maximum_diameter_change = scale_par[1]
+    asymetry_coef = scale_par[2]
+    location_id = scale_par[3]
+    steepness = scale_par[4]
+    x50 = scale_par[5]
+    
+	scale_factor                        = []
+	for i in range(number_of_contours): scale_factor.append(float(1))
+	if (length_id % 2 !=0): 
+		length_id -=1
+	half_num                        = int(length_id/2)
+	center_id                       = location_id
+	start_id                        = center_id - half_num
+	stop_id                         = start_id + length_id
+	center_dislocation              = int(half_num * asymetry_coef)
+	center_id                       = center_id + center_dislocation
+	number_of_proximal_contours     = center_id - start_id
+	number_of_distal_contours       = stop_id - center_id
+	print("Start ID: {0:d}".format(start_id))
+	print("Center ID: {0:d}".format(center_id))
+	print("Stop ID: {0:d}".format(stop_id))
+	prox_scale_factors              = []
+	dist_scale_factors              = []
+	print("Total contour numbers: {0:d}".format(number_of_contours))
+	print("Total modified contours: {0:d}".format(length_id))
+	print("Proximal contour numbers: {0:d}".format(number_of_proximal_contours))
+	print("Distal contour numbers: {0:d}".format(number_of_distal_contours))
+	for i in range(number_of_proximal_contours):
+		prox_scale_factors.append(sigmoid(maximum_diameter_change,number_of_proximal_contours,i+1,steepness,x50))
+	for s in range(number_of_distal_contours):
+		dist_scale_factors.append(sigmoid(maximum_diameter_change,number_of_distal_contours,s,steepness,x50))
+	# sort the scale lists
+	dist_scale_factors                       = np.sort(dist_scale_factors)
+	# sort in descending order
+	# dist_scale_factors                     = np.sort(dist_scale_factors)[::-1] 
+	print("Proximal scale factor list:")
+	print(prox_scale_factors)
+	print("Distal scale factor list:")
+	print(dist_scale_factors)
+	scale_factor_local                  	= np.concatenate((prox_scale_factors,dist_scale_factors),axis=None)
+	indx_counter                        	= 0
+	scale_factor				= scale_factor_insert(scale_factor,scale_factor_local,start_id,stop_id)
+	return scale_factor
+
+def read_ctgr_file(file_name, scale_par, contour_ids):
     
     # Remove 'format' tag from xml file.
     f = open(file_name, "r")
@@ -58,18 +138,21 @@ def read_ctgr_file(file_name, contour_scale, contour_ids):
 
     ## Create contour groups.
     #
+    
     contour_groups = []
     scaled_contour_groups = []
     for contour_group_t in root.iter('contourgroup'):
         path_name = contour_group_t.attrib["path_name"]
         contour_group = ContourGroup(path_name)
         scaled_contour_group = ContourGroup(path_name)
-
+        num_contours = len(contour_group_t)
+        print("number of contours: {}".format(num_contours))
+        scale_factors                       = scale_factor_test(num_contours,scale_par)
         for contour_t in contour_group_t.iter('contour'):
             cid = contour_t.attrib["id"]
             contour = Contour(cid)
             scaled_contour = Contour(cid)
-            scale = contour_ids[cid]
+            scale = scale_factors[cid]
 
             ## Iterate over control points. 
             #
