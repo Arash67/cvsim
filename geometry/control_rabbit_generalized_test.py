aa8f07e0d618e5642888a43ffb18f284083f3ea6
Arash GH <51331180+Arash67@users.noreply.github.com>
2021-10-24 22:02:59 -0500

    Create control_rabbit_generalized_test.py

---
+++ geometry/control_rabbit_generalized_test.py
@@ -0,0 +1,25 @@
+# NOTE: add Path from pathlib, add additional notes heare if needed
+from pathlib import Path
+# define the local home and repository directories plus out of source directory for outputs not needed to be uploaded to the source repository 
+home                    = str(Path.home())
+cvsim                   = home + "/github/cvsim/"
+cvsimout                = home + "/github/outofsource/cvsimout/"
+input_dir                    = "data/input/"
+output_dir                   = cvsimout
+
+ctgr_file_name  = input_dir + "control_rabbit_32181_S01AO.ctgr"
+vtp_file_name   = output_dir + "Case_1_AO_mesh_complete_exterior.vtp"
+
+scale_factor = 0.7
+scale_ids    = [5,6]
+# other local modules
+vmanip_dir                   = cvsim + "modules/"
+print("control_point_manipulation:")
+print(vmanip_dir)
+try:
+    sys.path.insert(1, vmanip_dir)
+except:
+    print("Can't find the modules/graphics package. this package is orginialy from simvascular repository: SimVascular-Tests > new-api-tests > graphics")
+import scale_ctgr as sc
+
+sc.main(ctgr_filename,vtp_file_name,scale_factor,scale_ids)
